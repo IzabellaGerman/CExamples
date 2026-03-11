cfc88993b63933093e08582296dd68da75bdbc80
Izabella German <izzigerman@gmail.com>
2026-03-11 10:01:17 +0100

    Types:

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Tom";
+            int age = 33;
+            bool isEmployed = false;
+            double weight = 78.65;
+
+            Console.WriteLine($"Name:  {name}");
+            Console.WriteLine($"Age:  {age}");
+            Console.WriteLine($"work:  {isEmployed}");
+            Console.WriteLine($"Weight:  {weight}");
+           
+
+        }
+    }
+}
