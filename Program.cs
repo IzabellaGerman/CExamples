50a1b11049f3abc8c9b4d1debacbdddc35371837
Izabella German <izzigerman@gmail.com>
2026-03-11 09:42:08 +0100

    literals

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Literaly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(true);     //true
+
+            Console.WriteLine(false);   //false
+
+            Console.WriteLine(-56);     //integer 
+
+            Console.WriteLine(0);       //integer
+
+            Console.WriteLine(0xFF);    //255
+
+            Console.WriteLine(0b11);    //3
+
+            Console.WriteLine(3.14);    //Pi
+
+            Console.WriteLine(1.2e-2);  //0.012
+
+            Console.WriteLine(3.2e3);   //3200
+
+        }
+    }
+}
