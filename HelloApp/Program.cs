eb7bb061e0580606cb120396246bd988e1080a35
Izabella German <izzigerman@gmail.com>
2026-03-11 09:19:33 +0100

    hello app

---
+++ HelloApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System; //подключаемое пространство имен
+
+namespace HelloApp //объявление нового пространства имен
+{
+    class Program // нового класса
+    {
+        static void Main(string[] args) //объявление нового метода
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}");
+           // Console.WriteLine("Hello World!");//действия метода
+        } //конец  объявления нового метода
+
+    } //конец объявление нового класса
+
+}// конец пространства имен
