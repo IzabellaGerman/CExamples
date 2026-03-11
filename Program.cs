93058c4029fdaacd7f8c53edd46311e8c3b71f51
Izabella German <izzigerman@gmail.com>
2026-03-11 09:30:30 +0100

    the variables

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using static System.Console;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name = "Izabella";
+            byte Age = 33;
+            bool IsEmpl = false;
+            float Weight = 53.2F;
+
+            WriteLine($"Your name: {Name}");
+            WriteLine($"Your age: {Age} years");
+            WriteLine($"Work: {IsEmpl}");
+            WriteLine($"Your weght: {Weight} kgs");
+            WriteLine($"Your name: {Name}; Your age: {Age} years; Work: {IsEmpl}; Your weght: {Weight} kgs.");
+            WriteLine("Your name: {0}; your age: {3} years; work: {1}; your weght: {2} kgs.", Name, IsEmpl, Weight, Age );
+            WriteLine("");
+
+            WriteLine("Enter Your name: ");
+            string name = ReadLine();
+
+            WriteLine("Enter your age: ");
+            byte age = Convert.ToByte(ReadLine());
+
+            WriteLine("Enter your heght: ");
+            double heidht = Convert.ToDouble(ReadLine());
+
+            WriteLine("Enter your weght, kgs: ");
+            double weight = Convert.ToDouble(ReadLine());
+
+            WriteLine($"Your data: Name - {name}, Age - {age} years, heght - {heidht} cm, weght - {weight} kg");
+        }
+    }
+}
