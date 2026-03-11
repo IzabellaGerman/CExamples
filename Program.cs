edece4ab4c7c7f986f8f51c32c4a77c6472b9009
Izabella German <izzigerman@gmail.com>
2026-03-11 10:04:15 +0100

    WriteLine

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ввод
+            string hello = "Hello world!";
+            Console.WriteLine(hello);
+            Console.WriteLine("Welcome to C#");
+            Console.WriteLine("Goodbye!");
+            Console.WriteLine(24.5);
+
+            string name = "Izabella";
+            byte age = 34;
+            double height = 170.5;
+
+            Console.WriteLine($"Name: {name} Age: {age} years Height: {height} m");
+            //Console.ReadKey();
+
+            //Есть другой способ вывода на консоль сразу нескольких значений
+            //закомментируй первый readkey, чтобы сработал второй вывод
+
+            Console.WriteLine("Name: {0} Age {2} years  Height {1} m", name, height, age);
+            //Console.ReadKey();
+
+            //вывод
+
+            Console.Write("Enter your name: ");
+            string name2 = Console.ReadLine();
+            Console.WriteLine($"Hi {name2}!");
+
+        }
+    }
+}
