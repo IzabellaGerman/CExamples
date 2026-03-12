3d35a01847f4b4984463164d871245fa891f8152
Izabella German <izzigerman@gmail.com>
2026-03-12 14:58:36 +0100

    Convert methos

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter youe height: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter your salary: ");
+            decimal salary = Convert.ToDecimal(Console.ReadLine());
+
+
+        }
+    }
+}
