8ff2c5c5534be539626d3cf81fccfd643feab6dd
Izabella German <izzigerman@gmail.com>
2026-03-16 09:03:12 +0100

    exchange the value of variables without using additional vaariables

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //exchange the value of variables without using additional vaariables
+            Console.WriteLine("Enter a:");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter b:");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+            Console.WriteLine($"a = {a} b = {b}");
+
+        }
+    }
+}
