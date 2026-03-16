6671293f24cd5b515d4c0943b2101bb3cf97d8eb
Izabella German <izzigerman@gmail.com>
2026-03-16 09:09:21 +0100

    exchange the value of variables without using additional vaariables

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter x:");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Odd:");
+            Console.WriteLine(Odd(x));
+            Console.ReadKey();
+
+        }
+
+        static bool Odd(int a)
+        {
+            return a % 2 != 0;
+
+        }
+    }
+}
