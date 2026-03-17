a5dc12624b883fbb00f6c3a44b7f3db0d0322809
Izabella German <izzigerman@gmail.com>
2026-03-17 15:44:08 +0100

    find a maximum of three numbers

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3, max;
+            Console.WriteLine("Enter number 1:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter number 2:");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter number 3:");
+            num3 = Convert.ToInt32(Console.ReadLine());
+
+
+            //option 1
+            if (num1 > num2 && num1 > num3 ) 
+            {
+                Console.WriteLine($"Max = {num1}");
+            }
+            else 
+            { if (num2 > num3)
+                {
+                    Console.WriteLine($"Max = {num2}");
+                }
+                else
+                {
+                    Console.WriteLine($"Max = {num3}");
+                }
+            };
+
+            //option 2
+            max = num1;
+            if (num2 > max)
+            {
+                max = num2;
+            };
+           
+            if (num3 > max)
+            {
+                max = num3;
+            };
+              
+            Console.WriteLine($"Max = {max}");
+
+
+
+
+        }
+    }
+}
