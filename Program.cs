e178162de26da4777634fcc0c13e18e2a1acc4ad
Izabella German <izzigerman@gmail.com>
2026-03-17 17:07:07 +0100

    Print all numbers in the range from 10 to 100 whose sum of the last two digits is equal to 10

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        //Print all numbers in the range from 10 to 100 whose sum of the last two digits is equal to 10:
+        //19, 28, 37, 46, 55, 64, 73, 82, 91
+        static bool sum_numbers(int i)
+        {
+            if ( i%10 + i/10%10 == 10) return true; else return false;
+        }
+        static void Main(string[] args)
+        {
+            int i;
+
+            for (i = 10; i <= 100; i++)
+            {
+                if (sum_numbers(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            
+        }
+    }
+}
