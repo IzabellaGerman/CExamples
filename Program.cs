5c09824798c102cdf79a7bc860f3defdd9f5e65a
Izabella German <izzigerman@gmail.com>
2026-03-20 13:13:28 +0100

    recursion

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static int Sum(int a)
+        {
+            int sm = 0;
+            int i = 0;
+
+            for (i = 0; i <= a; i++)
+            
+              if (i == 0)
+                sm = 1; 
+              else 
+                sm *= i;
+            return sm;
+        }
+
+        static void Main(string[] args)
+        {
+            int a = 0;
+
+            Console.WriteLine("Enter number: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Factorial: {Sum(a)}");
+        }
+    }
+}
