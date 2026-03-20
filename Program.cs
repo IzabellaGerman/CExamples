a602d0dbbb279c02d89dda6b6b426684c36e35df
Izabella German <izzigerman@gmail.com>
2026-03-20 13:00:53 +0100

    the multiplication table

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+        {   /*Write a program that displays the multiplication table on the console. */
+        static void Main(string[] args)
+        {
+            int i = 1;
+            int j = 1;
+           
+            for (i = 1; i <= 10; i++)
+            { for (j = i; j <= 10; j++)
+                {
+                    Console.WriteLine(value: $"{i} * {j} = {i * j}");
+                }
+                Console.WriteLine();
+             
+             }    
+
+        }
+    }
+}
