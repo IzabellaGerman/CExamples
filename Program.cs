39d834a4e216bb3e52e98004e2b544987e206930
Izabella German <izzigerman@gmail.com>
2026-03-26 19:18:46 +0100

    inversion

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using static System.Console;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mas = {9, -2, 0 ,3, 6, 8, -1};
+            foreach (int i in mas)
+            {
+                Write($"{i} ");
+            }
+
+            WriteLine();
+
+            int temp;
+            int n = mas.Length;
+            int k = n / 2;
+
+            for (int i = 0; i < k; i++)
+            {
+                temp = mas[i];
+                mas[i] = mas[n - i - 1];
+                mas[n - i - 1] = temp;
+            }
+
+            foreach (int i in mas) 
+            {
+                Write($"{i} ");
+            }
+
+            
+        }
+    }
+}
